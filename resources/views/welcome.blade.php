44ea59fcc8d3da5b0fadf9bde6f35592f3861106
Jose <josemiralles1991@gmail.com>
2020-09-06 17:35:31 -0400

    Project setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Instavel
                 </div>
 
                 <div class="links">
